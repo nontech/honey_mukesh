2d6d583b300dd8bdcc8abb63aac5470561c3a889
Mukesh Jaiswal <jais.mukesh@gmail.com>
2022-11-12 20:24:47 +0100

    Feature: navigation added to main app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello World</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
