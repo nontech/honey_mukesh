20a1acef28a53e69030ea9f70e430c848749843b
Mukesh Jaiswal <jais.mukesh@gmail.com>
2022-11-14 01:02:20 +0100

    Refactor: Quiz Screen made functional with modal

--- src/screens/course/Quiz.js
+++ src/screens/course/Quiz.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, View, StyleSheet} from 'react-native';
+import {RadioButton, Text} from 'react-native-paper';
+import {Modal, Portal} from 'react-native-paper';
 
 const Quiz = ({navigation}) => {
+  // radio button
+  const [value, setValue] = React.useState(null);
+
+  // container
+  const [visible, setVisible] = React.useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20};
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Screen 4: Quiz</Text>
+    <View>
+      <RadioButton.Group
+        onValueChange={newValue => setValue(newValue)}
+        value={value}>
+        <RadioButton.Item label="First item" value="first" />
+        <RadioButton.Item label="Second item" value="second" />
+        <RadioButton.Item label="Third item" value="third" />
+      </RadioButton.Group>
       <Button title="Back" onPress={() => navigation.goBack()} />
-      <Button title="Submit" onPress={showResult()} />
+      <Button title="Submit" onPress={showModal} />
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={containerStyle}>
+          <Text>Example Modal. Click outside this area to dismiss.</Text>
+          <Button
+            title="Next"
+            onPress={() => {
+              // hide the modal
+              hideModal();
+              // navigate to next screen
+              navigation.navigate('final');
+            }}
+          />
+        </Modal>
+      </Portal>
     </View>
   );
 };
 
-// a modal/popup with an action button
-function showResult() {
-  // action button should take to final screen
-}
+const styles = StyleSheet.create({
+  radioContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
 export {Quiz};
